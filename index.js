eb4756fbaf51fe79a20f9bbe24ed05345eb1af18
rtocastro <ricktorres@live.com>
2023-12-09 10:55:14 -0800

    added back the gitignore

--- index.js
+++ index.js
@@ -1,36 +1,53 @@
-const inquire = require("inquirer")
-const  jest = require("jest")
+const inquirer = require("inquirer")
 const fs = require("fs")
+const shapes = ["Triangle", "Circle", "Square"]
+const { Circle } = require('./lib/shapes')
 
 const questions = [
 
     {
-        type:'input',
-        name:'letters',
+        type: 'input',
+        name: 'letters',
         message: 'Please enter 3 letters for logo...'
     },
     {
-        type:'list',
-        name:'shapes',
-        message: 'Please choose a triangle, circle or square for logo...'
+        type: 'list',
+        name: 'choice',
+        message: " Please choose a shape from the list",
+        choices: shapes
     },
     {
-        type:'input',
-        name:'color',
-        message: 'Enter color or hex code to add color to logo...'
+        type: 'input',
+        name: 'colorshape',
+        message: 'Enter color or hex code to add color to logo shape...'
+    },
+    {
+        type: 'input',
+        name: 'colortext',
+        message: 'Enter color or hex code to add color to logo text...'
     },
 ];
 
-function writeToFile(fileName, data){
-    fs.writeFile(fileName, data, (err,result)=>{
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err, result) => {
         if (err) throw err;
         console.log('Logo has been made')
     })
 }
 
-function start(){
+function start() {
     inquirer.prompt(questions)
-    .then((answers)=> {
-        writeToFile('samplelogo.svg',??????? )
-    })
+        .then((answers) => {
+            let shape;
+
+            if (answers.choice === 'Circle'){
+                shape = new Circle(answers.letters, answers.colortext, answers.colorshape)
+            }
+
+
+
+            writeToFile('logo.svg',shape.render());
+        })
 }
+
+start();
